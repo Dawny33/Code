4b138930a1cd375692902850af10a4572e31a244
Jalem Raj Rohit <rajrohit@iitj.ac.in>
2014-07-01 12:47:16 +0530

    131A
    caps lock

--- Code_Forces/A_cAPS_lOCK.py
+++ Code_Forces/A_cAPS_lOCK.py
@@ -1,4 +1,16 @@
-T = raw_input()
+s = raw_input("")
 
-T = T.lower()
-print T[0].upper() + T[1:]
+def check(s, idx):
+    for i in range(idx, len(s)):
+        if (s[i].islower()):
+            return 0
+    return 1
+
+if check(s, 0):
+    ns = s.lower()
+elif check(s, 1):
+    ns = s[0].upper() + s[1:].lower()
+else:
+    ns = s
+
+print ns

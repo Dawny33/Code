edc0fd38c7deeab04613fd205319bbb9cbf3f8e5
Dawny33 <rajrohit@iitj.ac.in>
2014-08-15 19:52:30 +0530

    Clusterin Coeff.

--- LSNS_Lab Asignments/n_e_graph.py
+++ LSNS_Lab Asignments/n_e_graph.py
@@ -28,6 +28,22 @@ def graph(n,e):
         print summ    
     return m
 
-        
+
+#For calculating the average clustering coeffiecient.
+def l(a,i,r):
+    s=[b for b in r if a[i][b]]
+    k=len(s)
+    if k<2:
+        return 0
+    return 2.0*sum(map(lambda x:a[x[0]][x[1]],itertools.combinations(s,2)))/k/(k-1)
+
+def g(a):
+    n=len(a)
+    r=range(n)
+    return sum([l(a,i,r) for i in r])/n
+
+
+
+
 print graph(10,8)
 		

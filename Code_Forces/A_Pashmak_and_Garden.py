21f78e9bbad0b376da52eb087e6f8f951d95e996
Dawny33 <rajrohit@iitj.ac.in>
2014-08-15 22:19:39 +0530

    Completed

--- Code_Forces/A_Pashmak_and_Garden.py
+++ Code_Forces/A_Pashmak_and_Garden.py
@@ -10,6 +10,12 @@ elif (T[0]==T[2]) and (T[1]!=T[3]):
     p2 = T[3] - T[1]
     print str(T[0]+p2)+" "+str(T[1])+" "+str(T[2]+p2)+" "+str(T[3])
 
+elif (T[0]!=T[1]) and (T[0]!=T[2]) and (T[0]!=T[3]) and (T[1]!=T[2]) and (T[1]!=T[3]) and (T[2]!=T[3]):
+    print T[2]+" "+T[1]+" "+T[0]+" "+T[3]
+
+else:
+    print -1
+
 
 
 

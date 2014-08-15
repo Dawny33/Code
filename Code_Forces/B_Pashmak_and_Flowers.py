03a8a82d73843239dea5ee4be617dd93a9770818
Dawny33 <rajrohit@iitj.ac.in>
2014-08-15 23:00:18 +0530

    Completed (Unsuccesful)

---
+++ Code_Forces/B_Pashmak_and_Flowers.py
@@ -0,0 +1,31 @@
+T = raw_input()
+T = int()
+
+p = raw_input()
+p = map(int,p.split())
+
+arr = []
+arr.append(p)
+
+jk = max(arr)
+maxx = []
+for j in arr:
+    if j==jk:
+        maxx.append(jk)
+
+
+minn = []
+for k in arr:
+    if k == min(arr):
+        minn.append(k)
+
+
+if len(maxx)==len(minn):
+    print str(maxx[0]-minn[0])+" "+len(maxx)
+
+elif len(maxx)>len(minn):
+    print str(maxx[0]-minn[0])+" "+len(minn)
+
+elif len(maxx)<len(minn):
+    print str(maxx[0]-minn[0])+" "+len(maxx)
+

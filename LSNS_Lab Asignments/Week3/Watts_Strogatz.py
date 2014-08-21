9d27ad8621dd026f5810588911b2ad2116336b31
Jalem Raj Rohit <rajrohit@iitj.ac.in>
2014-08-21 14:35:35 +0530

    edited the generator

--- LSNS_Lab Asignments/Week3/Watts_Strogatz.py
+++ LSNS_Lab Asignments/Week3/Watts_Strogatz.py
@@ -1,27 +1,23 @@
 from networkx import *
+import numpy as np
 import matplotlib.pyplot as plt
 no_nodes=1000
 pp = []
 gg = []
 
 #Generator for 0 to 1.
-def float_range(start, end, increment):
-    int_start = int(start / increment)
-    int_end = int(end / increment)
-    for i in range(int_start, int_end):
-        print i * increment
 
 #print float_range(0,1,0.01)
 
 
-
-for p in float_range(0,1,0.01):
-    g = watts_strogatz_graph(no_nodes, 4, p)
+a=np.linspace(0,1,100)
+for p in a:
+    g= watts_strogatz_graph(no_nodes, 4, p)
     pp.append(density(g))
     gg.append(average_clustering(g))
 #print density(g), average_clustering(g)
-print pp,gg
-plt.plot(o,gg)
+#print pp,gg
+plt.plot(a,gg)
 plt.show()
-plt.plot(o,pp)
+plt.plot(a,pp)
 plt.show()

fa978a6a242f58ce4e86b5babf4fbe56f270c0e9
Dawny33 <rajrohit@iitj.ac.in>
2014-08-21 12:21:47 +0530

    done

--- LSNS_Lab Asignments/Week3/Watts_Strogatz.py
+++ LSNS_Lab Asignments/Week3/Watts_Strogatz.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 no_nodes=1000
 pp = []
 gg = []
+o = [ 0.1, 0.2, 0.3]
 for p in [ 0.1, 0.2, 0.3]:
     g = watts_strogatz_graph(no_nodes, 4, p)
     pp.append(density(g))
@@ -12,6 +13,10 @@ for p in [ 0.1, 0.2, 0.3]:
     #print density(g), average_clustering(g)
 print pp,gg
 
-plt.plot(pp,gg)
+plt.plot(o,gg)
+
 
 plt.show() 
+
+plt.plot(o,pp)
+plt.show()

266e040812c13f0a87a692a6e3efb3dc1830a919
Dawny33 <rajrohit@iitj.ac.in>
2014-08-21 12:10:42 +0530

    Improved

--- LSNS_Lab Asignments/Week3/Watts_Strogatz.py
+++ LSNS_Lab Asignments/Week3/Watts_Strogatz.py
@@ -1,5 +1,16 @@
 from networkx import *
+import matplotlib.pyplot as plt
+
 no_nodes=1000
+pp = []
+gg = []
 for p in [ 0.1, 0.2, 0.3]:
     g = watts_strogatz_graph(no_nodes, 4, p)
-    print density(g), average_clustering(g) 
+    pp.append(density(g))
+    gg.append(average_clustering(g))
+    
+    #print density(g), average_clustering(g)
+print pp,gg
+plot(pp,gg)
+
+plt.show() 

af81928d36b2f607cd086fd0e661d19f78017b02
Dawny33 <rajrohit@iitj.ac.in>
2014-08-24 23:36:37 +0530

    96A
    Easy as fuck, ain't it?

--- Code_Forces/A_Football.py
+++ Code_Forces/A_Football.py
@@ -1,14 +1,5 @@
-T = raw_input()
-
-flag = 0
-#first = 0
-op = 0
-for i in range(flag+1,len(T)):
-    if T[i] == flag:
-        op += 1
-    else:
-        flag = i
-if op>=7:
-    print "YES"
+players = raw_input()
+if '0000000' in players or '1111111' in players:
+    print('YES')
 else:
-    print "NO"    
+    print('NO')

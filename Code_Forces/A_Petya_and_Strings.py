970de3a8ba9c1a0373ddf440be7cf4a0fb905f46
Dawny33 <rajrohit@iitj.ac.in>
2014-08-28 00:56:30 +0530

    112A
    Petya and strings

--- Code_Forces/A_Petya_and_Strings.py
+++ Code_Forces/A_Petya_and_Strings.py
@@ -1,21 +1,6 @@
+
+
 T = raw_input()
 P = raw_input()
-P = P.lower()
 
-sum1 = 0
-for i in range(len(T)):
-    sum1 += ord(T[i])
-
-sum2 = 0
-for j in range(len(P)):
-    sum2 += ord(P[j])
-
-
-if sum1<sum2:
-    print "-1"
-
-elif sum1>sum2:
-    print "1"
-
-else:
-    print "0"
+print str(cmp(T.lower(), P.lower()))

2fe729b9fcdf11950f3077471f09f14d6081bc28
Dawny33 <rajrohit@iitj.ac.in>
2014-08-29 23:20:55 +0530

    Change in the module used

--- LSNS_Lab Asignments/Week3 and 4/Graph_CPL.m
+++ LSNS_Lab Asignments/Week3 and 4/Graph_CPL.m
@@ -4,7 +4,7 @@ ag = zeros;
 for beta = 0:.0001:1
     for j = 1:100
         C = small_world(n,k,beta);
-        [ag1(j), bg] = avgClusteringCoefficient(full(C));
+        [ag1(j), bg] = charpathlength(full(C));
         j=j+1;
     end
     ag(i)=mean(ag1);

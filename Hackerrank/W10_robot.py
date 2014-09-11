3c75e16a234e7014decc290f36324e804c498607
Dawny33 <rajrohit@iitj.ac.in>
2014-09-12 03:49:31 +0530

    Done

--- Hackerrank/W10_robot.py
+++ Hackerrank/W10_robot.py
@@ -6,13 +6,14 @@ def go(step,energy):
     score = 0
     if step==N:
         #print V
-        score += V[step]
+        score += V[step-1]
+        return score
     else:
         way = random.randint(1,2)
         if way==1:
-            score += V[step]
+            score += V[step-1]
         else:
-            energy = P[step]
+            energy = P[step-1]
         if energy>0:
             go(step+1, energy-1)
         else:
@@ -28,5 +29,9 @@ for _ in range(N):
     v,p = map(int, raw_input().split())
     V = V + [v]
     P = P + [p]
-go_arr.append(go(v-1,p-1))
-print sum(go_arr)
+
+for i in range(0,len(V)-1):
+    go_arr.append(go(V[i],P[i]))
+
+print max(go_arr)+N
+

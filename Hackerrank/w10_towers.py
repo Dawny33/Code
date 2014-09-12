11b65f116bfad4b58771658a9a692560aa51c9ee
Dawny33 <rajrohit@iitj.ac.in>
2014-09-12 20:03:08 +0530

    edited and not done

--- Hackerrank/w10_towers.py
+++ Hackerrank/w10_towers.py
@@ -1,11 +1,19 @@
+import itertools
+
 N = int(input())
 
 K = int(input())
 
+k1_arr = []
 k1 = map(int,raw_input().split())
 
-if(len(k1)<=K):
-    if K==1 and k1[0]==1:
-        print 2
-    else:
-        print k1[0]*K
+for i in range(len(k1)):
+    k1_arr.append(k1[i])
+
+
+#print k1_arr
+if(len(k1_arr)<=K):
+    kk = list(itertools.permutations(k1_arr, K))
+    print kk
+    print kk[1]
+        

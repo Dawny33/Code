4ee2308c806d37e95a2944a482ea9505ccb92760
Dawny33 <rajrohit@iitj.ac.in>
2014-09-14 00:42:47 +0530

    Optimized code, still not accepting
    maybe Hackerrank's problem

--- Hackerrank/CSIndia/bigger_greater.py
+++ Hackerrank/CSIndia/bigger_greater.py
@@ -1,21 +1,23 @@
+
 from itertools import permutations
 
 T = int(input())
 
-
 for _ in range(T):
     s = raw_input()
     set_arr = []
-    perms = [''.join(p) for p in permutations(s)]
-    for j in set(perms):
-        set_arr.append(j)
-    sor_set_arr = sorted(set_arr)
+    perms = [''.join(p) for p in permutations(s) if p>s]
+#    for j in set(perms):
+#        set_arr.append(j)
+#    sor_set_arr = qsort1(set_arr)
     fin_arr = []
-    for k in range(len(sor_set_arr)):
-        if sor_set_arr[k]>s:
-            fin_arr.append(sor_set_arr[k])
+    for k in range(len(perms)):
+        if perms[k]>s:
+            fin_arr.append(perms[k])
     if len(fin_arr)>0:
         print fin_arr[0]
     else:
         print "no answer"
+
+
 

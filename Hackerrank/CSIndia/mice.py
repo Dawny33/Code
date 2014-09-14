8542ae6c407209f5eb7e330ebeeb8898e3a6e927
Dawny33 <rajrohit@iitj.ac.in>
2014-09-14 13:48:34 +0530

    Mice qn
    Very naive approach

---
+++ Hackerrank/CSIndia/mice.py
@@ -0,0 +1,13 @@
+T = int(input())
+
+for _ in range(T):
+    num = int(input())
+    if num!=0:
+        arr = []
+        t1 = map(int, raw_input().split())
+        t2 = map(int, raw_input().split())
+        if len(t1)==num and len(t2)==num:
+            for i in range(num):
+                arr.append(abs(t1[i]-t2[i]))
+            print arr
+            print max(arr)

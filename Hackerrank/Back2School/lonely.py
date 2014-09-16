5be88ba690db736e8f66ec27ad01a4d0cd7bda83
Dawny33 <rajrohit@iitj.ac.in>
2014-09-16 18:44:39 +0530

    Ha ha  It's done, baby!!

--- Hackerrank/Back2School/lonely.py
+++ Hackerrank/Back2School/lonely.py
@@ -3,9 +3,9 @@ from collections import Counter
 T = int(input())
 
 p = map(int, raw_input().split(" "))
+ans = 0
 if len(p) == T:
     c = Counter(p)
     for i in range(len(c.keys())):
-        if c.keys()[i]==1:
-            print c.values()[i]
-        
+        if c.values()[i]==1:
+            print c.keys()[i]        

c088bb5094a03d553ed80d72259ecd5962603505
Dawny33 <rajrohit@iitj.ac.in>
2014-09-17 23:32:52 +0530

    Intermediate commit

--- Hackerrank/CSIndia/Q-2/pin_prob.py
+++ Hackerrank/CSIndia/Q-2/pin_prob.py
@@ -1,13 +1,25 @@
 
 T = int(input())
 
+def gcd(a,b):
+ if a < b: a,b = b,a
+ while a:
+  a,b = b,a%b
+ return b
+
+def lcm(a,b):
+ return (a*b)/gcd(a,b)
+
+
+
 for _ in range(T):
     s1 = map(int, raw_input().split())
     s2 = map(int, raw_input().split())
     count = 0
     if len(s2)==s1[1]:
-        for i in range(s1[0]):
-        
-            
-                    
-    print count
+        i = 0
+        while len(s2)>2:
+            a = s2.pop(i)
+            b = s2.pop(i+1)
+            s2.append(lcm(a,b))
+        print s2

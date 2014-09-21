8b39ce02519fe22dd89070766a692e1aec95c25e
Dawny33 <rajrohit@iitj.ac.in>
2014-09-21 07:53:39 +0530

    edited

--- Hackerrank/sep_infinitum/circle_tria.py
+++ Hackerrank/sep_infinitum/circle_tria.py
@@ -6,21 +6,20 @@ def disin(x,y,x_center,y_center,radius):
 def disout(x,y,x_center,y_center,radius):
     return (((x - x_center)**2 + (y - y_center)**2) > radius**2)
 
-
 while(T):
     T -= 1
-    s = map(int,raw_input().split())
-    t1 = map(int,raw_input().split())
-    t2 = map(int,raw_input().split())
-    t3 = map(int,raw_input().split())
+    xc,yc,r = map(int,raw_input().split())
+    x1,y1 = map(int,raw_input().split())
+    x2,y2 = map(int,raw_input().split())
+    x3,y3 = map(int,raw_input().split())
 
     #Condition for all in
-    if (disin(t1[0],t1[1],s[0],s[1], s[2])==True) and (disin(t2[0],t2[1],s[0],s[1], s[2])==True) and (disin(t3[0],t3[1],s[0],s[1], s[2])==True):
+    if (disin(x1,y1,xc,yc,r)==True) and (disin(x2,y2,xc,yc,r)==True) and (disin(x3,y3,xc,yc,r)==True):
         print "NO"
 
     #Condition for all out
-    elif (disout(t1[0],t1[1],s[0],s[1], s[2])==True) and (disout(t2[0],t2[1],s[0],s[1], s[2])==True) and (disout(t3[0],t3[1],s[0],s[1], s[2])==True):
+    elif (disout(x1,y1,xc,yc,r)==True) and (disout(x2,y2,xc,yc,r)==True) and (disout(x3,y3,xc,yc,r)==True):
         print "NO"
-
+ 
     else:
         print "YES"

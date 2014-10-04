8d63cbc7cbe5ef756351cc41ec0cfc196d4de0c2
Dawny33 <rajrohit@iitj.ac.in>
2014-10-04 17:53:22 +0530

    3 problem completed
    Challenge time up!

--- HackerEarth/Glydel Hiring/brute_force.py
+++ HackerEarth/Glydel Hiring/brute_force.py
@@ -1,21 +1,12 @@
 def is_cube(n):
-
     if isinstance(n, int) and n > 0:
-
         root = 0
-
         while root**3 < n:
-
             root += 1
-
             if root**3 == n:
-
                 return root
-
                 break
-
         else:
-
             return False
 
 

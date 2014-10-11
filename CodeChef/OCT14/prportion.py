3b290bdebd3b900bbf1d28c6f8dac60d02e5c3da
Dawny33 <rajrohit@iitj.ac.in>
2014-10-12 00:21:55 +0530

    Failure attempt at Oct challenge 2

--- CodeChef/OCT14/prportion.py
+++ CodeChef/OCT14/prportion.py
@@ -11,6 +11,31 @@ while(T):
     sumg = sum(g)
     sumb = sum(b)
 
+
+    while(M):
+        M -= 1
+        if sumr>sumg and sumr>sumb:
+            rn = [x / 2 for x in r]
+            r = rn
+            sumr = sum(r)
+        #print max(r+g+b)
+
+        if sumg>sumr and sumg>sumb:
+            gn = [y / 2 for y in g]
+            g = gn
+            sumg = sum(g)
+        #print max(r+g+b)
+
+        if sumb>sumg and sumb>sumr:
+            bn = [z / 2 for z in b]
+            b = bn
+            sumb = sum(b)
+        #print max(r+g+b)
+
+    print max(r+g+b)
+
+    
+
     
     
         

91c5405c2ffc3c3bebe89967230fe7f194d571f5
Dawny33 <rajrohit@iitj.ac.in>
2014-10-18 21:01:04 +0530

    Added two more codes

---
+++ CodeChef/CodeMasters/CM1404.py
@@ -0,0 +1,9 @@
+T = input()
+
+while(T):
+    T -= 1
+    s = input()
+    if s%2==0:
+        print s/2
+    else:
+        print s/2 + 1
--- Hackerrank/oct_infinitum/eight.py
+++ Hackerrank/oct_infinitum/eight.py
@@ -1,24 +1,37 @@
 T = input()
 
-
-
-def power(b, p):
-    res = 1
-    while p:
-        if p & 1:
-            res *= b
-        b *= b
-        p >>= 1
-
-    return res
-
 while(T):
     T -= 1
-    n,a,b,m = [int(i) for i in raw_input().split()]
-    summ = 0
-    i = 0
-    while(i<=n):
-        summ += power(i,a)*power(b,i)
-        i += 1
 
-    print summ%m
+    N,K,x,y = [int(i) for i in raw_input().split()]
+    while(N):
+        N -= 1
+        while(K):
+            s = [i for i in raw_input().split()]
+            if s[0]=="T":
+                a,b = int(s[1]),int(s[2])
+                x = a+x
+                y = y+x
+            if s[0]=="S":
+                c = int(s[1])
+                x = c*x
+                y = c*y
+            if s[0]=="R":
+                a,b = int(s[1]),int(s[2])
+                x = a*x + b*y
+                y = a*y - b*x
+            if s[0]=="F":
+                if s[1]=="X":
+                    y = -x
+                else:
+                    x = -x
+            if s[0]=="I":
+                if x==0 and y==0:
+                    print "WONDERLAND"
+                else:
+                    x = (x/(x**2 + y**2))
+                    y = (y/(x**2 + y**2))
+            
+            K -= 1
+    
+                

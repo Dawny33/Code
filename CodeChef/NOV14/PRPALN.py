d0886b6019ff7700aa507376661512106bd36b52
Dawny33 <rajrohit@iitj.ac.in>
2014-11-08 08:52:49 +0530

    attempting the PRPALN problem

---
+++ CodeChef/NOV14/PRPALN.py
@@ -0,0 +1,27 @@
+def isPalindrome(String):
+    stringSize = len(String)
+    start = 0
+    end = stringSize - 1
+    while start < end and String[start] == String[end]:
+        start += 1
+        end -= 1
+    if start < end:
+        return False
+    else:
+        return True
+
+def isKPalindrome(String, k):
+    if k > 0:
+        for i in range(len(String)):
+            subString = String[:i] + String[i+1:]
+            if isKPalindrome(subString, k-1) == True:
+                return True
+            return False
+    elif k == 0:
+        return isPalindrome(String)
+    else:
+        print 'Error number k'
+        
+    return False 
+
+print isKPalindrome(raw_input(),1)

2a366f8d4beb1172accd8dd5a778dfe0eaba4e36
Dawny33 <rajrohit@iitj.ac.in>
2014-11-12 09:09:12 +0530

    Codeforces round 277 first prob. solved

--- CodeChef/NOV14/POWERMUL.py
+++ CodeChef/NOV14/POWERMUL.py
@@ -1,16 +1,16 @@
+import sys
 T = input()
 
 def func(d):
-    prod = 1
-    for j in range(1,d+1):
-        prod *= pow(j,j)
-    return prod
-
+    if d == 1:
+        return 1
+    else:
+        return func(d-1)*pow(d,d)
 
 
 while(T):
     T -= 1
-    N,M,Q = [int(i) for i in raw_input().split()]
+    N,M,Q = [int(i) for i in sys.stdin.readline().split()]
 
     while(Q):
         Q -= 1
---
+++ Code_Forces/Contest 277/A.py
@@ -0,0 +1,6 @@
+T = input()
+
+if T%2==0:
+    print T/2
+else:
+    print ((T-1)/2)-T

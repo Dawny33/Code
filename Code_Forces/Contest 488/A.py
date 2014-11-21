a981ad57a4a209405c82b38296e59f3c8ae307fa
Dawny33 <rajrohit@iitj.ac.in>
2014-11-21 23:42:41 +0530

    Contest 488

---
+++ Code_Forces/Contest 488/A.py
@@ -0,0 +1,10 @@
+S = input()
+
+i = S+1
+while True:
+    if "8" in str(i):
+        print i-S
+        break
+    else:
+        i += 1
+
---
+++ Code_Forces/Contest 488/B.py
@@ -0,0 +1,12 @@
+T = input()
+
+arr = []
+
+while(T):
+    arr.append(input())
+
+if T == 4:
+    print "YES"
+
+if T == 3:
+    
